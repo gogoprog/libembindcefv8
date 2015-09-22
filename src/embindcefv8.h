0efadd3c583d0c7f4313835b0f9b81b6177e184d
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-09-22 13:46:15 +0200

    Wip

--- src/embindcefv8.h
+++ src/embindcefv8.h
@@ -1,5 +1,50 @@
 #pragma once
 
 #ifdef EMSCRIPTEN
+    #include <emscripten.h>
     #include <emscripten/bind.h>
 #endif
+
+namespace embindcefv8
+{
+    template<class T>
+    class ValueObject
+    {
+    public:
+        ValueObject(const char *_name)
+        {
+            strcpy(name, _name);
+            vo = new emscripten::value_object<T>(_name);
+        }
+
+        ValueObject & constructor()
+        {
+            T ( * func )() =
+                []() -> T
+                {
+                    return T();
+                };
+
+            #ifdef EMSCRIPTEN
+                emscripten::function( name, func );
+            #endif
+
+            return *this;
+        }
+
+    private:
+        #ifdef EMSCRIPTEN
+            char
+                name[128];
+            emscripten::value_object<T>
+                * vo;
+        #endif
+    };
+
+    void execute(const char *str)
+    {
+        #ifdef EMSCRIPTEN
+            emscripten_run_script(str);
+        #endif
+    }
+}
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,21 @@
 #include "embindcefv8.h"
 #include <iostream>
 
+struct TestStruct
+{
+    float
+        floatMember;
+};
+
 int main(int argc, char* argv[])
 {
     std::cout << "embindcefv8 - tests" << std::endl;
+
+    embindcefv8::ValueObject<TestStruct>("TestStruct")
+        .constructor();
+
+
+    embindcefv8::execute("var test = Module.TestStruct();");
+
     return 0;
 }
