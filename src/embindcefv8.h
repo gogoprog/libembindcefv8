677d7ab613284b9e10ab3439fca85ca0ca1012e4
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-05-02 13:13:41 +0200

    Updated ConstructorInvokers

--- src/embindcefv8.h
+++ src/embindcefv8.h
@@ -484,7 +484,21 @@ namespace embindcefv8
                 return new T(
                     ValueConverter<A0>::get(*arguments[0]),
                     ValueConverter<A1>::get(*arguments[1]),
-                    ValueConverter<A1>::get(*arguments[2])
+                    ValueConverter<A2>::get(*arguments[2])
+                    );
+            }
+        };
+
+        template<typename T, typename A0, typename A1, typename A2, typename A3>
+        struct ConstructorInvoker<T, A0, A1, A2, A3>
+        {
+            static T * call(const CefV8ValueList& arguments)
+            {
+                return new T(
+                    ValueConverter<A0>::get(*arguments[0]),
+                    ValueConverter<A1>::get(*arguments[1]),
+                    ValueConverter<A2>::get(*arguments[2]),
+                    ValueConverter<A3>::get(*arguments[3])
                     );
             }
         };
